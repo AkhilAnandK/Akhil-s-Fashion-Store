02583ad08dc211b638c06a4c3b3f016968e5d428
Akhilanand Kundurthi <kundurthiakhil@gmail.com>
2023-02-20 12:03:45 -0600

    created the shipping form

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -4,6 +4,7 @@ import { Formik } from "formik";
 import { useState } from "react";
 import * as yup from "yup";
 import { shades } from "../../theme";
+import Shipping from "./Shipping";
 
 const initialValues = {
   billingAddress: {
@@ -32,55 +33,55 @@ const initialValues = {
 };
 
 const checkoutSchema = [
-    yup.object().shape({
-      billingAddress: yup.object().shape({
-        firstName: yup.string().required("required"),
-        lastName: yup.string().required("required"),
-        country: yup.string().required("required"),
-        street1: yup.string().required("required"),
-        street2: yup.string(),
-        city: yup.string().required("required"),
-        state: yup.string().required("required"),
-        zipCode: yup.string().required("required"),
+  yup.object().shape({
+    billingAddress: yup.object().shape({
+      firstName: yup.string().required("required"),
+      lastName: yup.string().required("required"),
+      country: yup.string().required("required"),
+      street1: yup.string().required("required"),
+      street2: yup.string(),
+      city: yup.string().required("required"),
+      state: yup.string().required("required"),
+      zipCode: yup.string().required("required"),
+    }),
+    shippingAddress: yup.object().shape({
+      isSameAddress: yup.boolean(),
+      firstName: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
       }),
-      shippingAddress: yup.object().shape({
-        isSameAddress: yup.boolean(),
-        firstName: yup.string().when("isSameAddress", {
-          is: false,
-          then: yup.string().required("required"),
-        }),
-        lastName: yup.string().when("isSameAddress", {
-          is: false,
-          then: yup.string().required("required"),
-        }),
-        country: yup.string().when("isSameAddress", {
-          is: false,
-          then: yup.string().required("required"),
-        }),
-        street1: yup.string().when("isSameAddress", {
-          is: false,
-          then: yup.string().required("required"),
-        }),
-        street2: yup.string(),
-        city: yup.string().when("isSameAddress", {
-          is: false,
-          then: yup.string().required("required"),
-        }),
-        state: yup.string().when("isSameAddress", {
-          is: false,
-          then: yup.string().required("required"),
-        }),
-        zipCode: yup.string().when("isSameAddress", {
-          is: false,
-          then: yup.string().required("required"),
-        }),
+      lastName: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
+      }),
+      country: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
+      }),
+      street1: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
+      }),
+      street2: yup.string(),
+      city: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
+      }),
+      state: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
+      }),
+      zipCode: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
       }),
     }),
-    yup.object().shape({
-      email: yup.string().required("required"),
-      phoneNumber: yup.string().required("required"),
-    }),
-  ];
+  }),
+  yup.object().shape({
+    email: yup.string().required("required"),
+    phoneNumber: yup.string().required("required"),
+  }),
+];
 
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
@@ -109,7 +110,64 @@ const Checkout = () => {
           onSubmit={handleFormSubmit}
           initialValues={initialValues}
           validationSchema={checkoutSchema[activeStep]}
-        ></Formik>
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+            setFieldValue,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              {isFirstStep && (
+                <Shipping
+                  values={values}
+                  errors={errors}
+                  touched={touched}
+                  handleBlur={handleBlur}
+                  handleChange={handleChange}
+                  setFieldValue={setFieldValue}
+                />
+              )}
+              <Box display="flex" justifyContent="space-between" gap="50px">
+                {!isFirstStep && (
+                  <Button
+                    fullWidth
+                    color="primary"
+                    variant="contained"
+                    sx={{
+                      backgroundColor: shades.primary[200],
+                      boxShadow: "none",
+                      color: "white",
+                      borderRadius: 0,
+                      padding: "15px 40px",
+                    }}
+                    onClick={() => setActiveStep(activeStep - 1)}
+                  >
+                    Back
+                  </Button>
+                )}
+                <Button
+                  fullWidth
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                  sx={{
+                    backgroundColor: shades.primary[400],
+                    boxShadow: "none",
+                    color: "white",
+                    borderRadius: 0,
+                    padding: "15px 40px",
+                  }}
+                >
+                  {!isSecondStep ? "Next" : "Place Order"}
+                </Button>
+              </Box>
+            </form>
+          )}
+        </Formik>
       </Box>
     </Box>
   );
