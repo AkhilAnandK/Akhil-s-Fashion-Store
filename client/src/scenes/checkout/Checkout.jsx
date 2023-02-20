6623b5b0ab2d6eea15aabcf400250d3e29b999d0
Akhilanand Kundurthi <kundurthiakhil@gmail.com>
2023-02-20 13:28:39 -0600

    modified checkout for payment

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -8,7 +8,7 @@ import Shipping from "./Shipping";
 import Payment from "./Payment";
 import { loadStripe } from "@stripe/stripe-js";
 const stripePromise = loadStripe(
-    "pk_test_51MderZHsdaI3pnh9DadMH36NeIvaJp523AWFZGynfp4YYl4m1PGg18dmRGGa9VAHrGVAhPowGrkSmcm8C5h8Uns700GSuwQAB0"
+  "pk_test_51MderZHsdaI3pnh9DadMH36NeIvaJp523AWFZGynfp4YYl4m1PGg18dmRGGa9VAHrGVAhPowGrkSmcm8C5h8Uns700GSuwQAB0"
 );
 
 const initialValues = {
@@ -112,7 +112,27 @@ const Checkout = () => {
     actions.setTouched({});
   };
 
-  async function makePayment(values) {}
+  async function makePayment(values) {
+    const stripe = await stripePromise;
+    const requestBody = {
+      userName: [values.firstName, values.lastName].join(" "),
+      email: values.email,
+      products: cart.map(({ id, count }) => ({
+        id,
+        count,
+      })),
+    };
+
+    const response = await fetch("http://localhost:1337/api/orders", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(requestBody),
+    });
+    const session = await response.json();
+    await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+  }
 
   return (
     <Box width="80%" m="100px auto">
